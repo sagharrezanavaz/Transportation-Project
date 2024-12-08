64e8bbf680641a88bbf5a8f1b67379f98bdb5756
sagharrezanavaz <150578639+sagharrezanavaz@users.noreply.github.com>
2024-12-08 16:04:17 +0330

    preprocessing without filling null

--- Project.py
+++ Project.py
@@ -7,6 +7,7 @@ from sklearn.feature_selection import RFE, SelectKBest, chi2
 from sklearn.ensemble import HistGradientBoostingClassifier, HistGradientBoostingRegressor, RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 df=pd.read_csv('2021_Green_Taxi_Trip_Data.csv')
+df.describe()
 irrelevant_columns = ['RatecodeID', 'store_and_fwd_flag', 'improvement_surcharge',
                     'ehail_fee', 'mta_tax', 'extra', 'fare_amount',
                     'congestion_surcharge']
@@ -19,5 +20,12 @@ df['trip_type'] = df['trip_type'].astype(object)
 df.loc[(df['tip_amount'] > 0) & (df['payment_type'].isnull()), 'payment_type'] = 1
 df.loc[df['passenger_count'] > 4, 'payment_type'] = 'unknown'
 df.loc[df['passenger_count'] > 4, 'trip_type'] = 'dispatch'
+df.dropna(subset=['VendorID'], inplace=True)
+df['pickup_datetime'] = pd.to_datetime(df['lpep_pickup_datetime'], format='%m/%d/%Y %I:%M:%S %p')
+df = df[df['pickup_datetime'].dt.year == 2021]
+percent_missing = df.isnull().sum() * 100 / len(df)
+missing_value_df = pd.DataFrame({'column_name': df.columns,
+                                 'percent_missing': percent_missing})
+print(missing_value_df)
 
 
